c993609d12e79ff0b22af210d1b72504ec0fdbd1
AllenKirby <130225133+AllenKirby@users.noreply.github.com>
2024-02-22 13:33:41 +0800

    fonts and sizes adjusted

--- src/components/container.jsx
+++ src/components/container.jsx
@@ -34,32 +34,32 @@ const Container = () =>{
     };
 
     return(
-        <section className="h-auto w-auto bg-white rounded-2xl shadow-2xl shadow-white opacity-40 p-5">
+        <section className="md:w-auto h-auto w-full bg-white rounded-2xl shadow-2xl shadow-white opacity-40 p-5">
             <form onSubmit={getWeather} className="flex items-center justify-center p-4" >
-                <input type="text" name="location" value={location} onChange={handleInputChange} placeholder="Enter a Location" required className="w-80 py-3 mx-4 font-poppins text-lg outline-none border-b-black border-b-2"/>
-                <button type="submit" className="border-2 border-black rounded-xl p-3"><IoMdSearch size={30} color="black"/></button>
+                <input type="text" name="location" value={location} onChange={handleInputChange} placeholder="Enter a Location" required className="w-80 py-3 mx-4 font-poppins text-base outline-none border-b-black border-b-2"/>
+                <button type="submit" className="border-2 border-black rounded-xl p-3"><IoMdSearch size={20} color="black"/></button>
             </form>
             <div className="w-full h-auto">
                 {error && <p className="text-red-500 font-poppins text-sm text-center">{error}</p>}
                 {weather && (
                     <>
                         <article className="w-full p-3 text-center">
-                            <p className="font-poppins text-7xl font-semibold my-2">{weather.main && Math.round(weather.main.temp)}°C</p>
-                            <p className="font-poppins text-3xl">{weather.name}</p>
+                            <p className="font-poppins text-6xl font-semibold my-2">{weather.main && Math.round(weather.main.temp)}°C</p>
+                            <p className="font-poppins text-2xl">{weather.name}</p>
                         </article>
-                        <section className="w-full flex flex-row py-10 px-5">
+                        <section className="w-full flex flex-row items-center justify-between py-10 px-5">
                             <div className="flex mr-10">
-                                <div><WiHumidity size={80}/></div>
+                                <div><WiHumidity size={70}/></div>
                                 <div className="py-2">
-                                    <p className="font-poppins text-xl">{weather.main && weather.main.humidity}%</p>
-                                    <p className="font-poppins text-2xl">Humidity</p>
+                                    <p className="font-poppins text-lg">{weather.main && weather.main.humidity}%</p>
+                                    <p className="font-poppins text-xl">Humidity</p>
                                 </div>
                             </div>
                             <div className="flex ml-10">
-                                <div><PiWindBold size={70}/></div>
+                                <div><PiWindBold size={60}/></div>
                                 <div className="py-2 ml-2">
-                                    <p className="font-poppins text-xl">{weather.wind && weather.wind.speed}km/h</p>
-                                    <p className="font-poppins text-2xl">Wind</p>
+                                    <p className="font-poppins text-lg">{weather.wind && weather.wind.speed}km/h</p>
+                                    <p className="font-poppins text-xl">Wind</p>
                                 </div>
                             </div>
                         </section>
